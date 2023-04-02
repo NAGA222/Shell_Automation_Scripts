7b167e7bae583e99f292326b518f3b4f8340440f
“NAGA222” <bhushanam2222@gmail.com>
2023-04-02 16:16:16 +0530

    jira account api comment code commit

--- Jira_Rest_Api_comment_update.sh
+++ Jira_Rest_Api_comment_update.sh
@@ -3,7 +3,6 @@ version="1.2.3.0"
 for line in $(cat jiraid.txt)
 do
 #curl -X PUT -u "usergmail:token --data '{"update":{"labels":[{"add":"Storage-Issue"}]}}' -H "Content-Type: application/json" https://therealyou.atlassian.net/rest/api/3/issue/DP-1
-#curl -X PUT -u "usergmail:token --data '{"update":{"labels":[{"add":"Storage-Issue"}]}}' -H "Content-Type: application/json" https://therealyou.atlassian.net/rest/api/3/issue/$line
 
 curl -X PUT -u "usergmail:token" --data '{"update":{"comment":[{"add":{"body": "Sample New Comment adding using Rest API"}}]}}' -H "Content-Type: application/json" https://therealyou.atlassian.net/rest/api/2/issue/$line
 done
